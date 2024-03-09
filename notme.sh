0b9333a28669444de64452a0011ee6f3cc4059da
gdarquie <gaetan.darquie@gmail.com>
2024-03-09 11:17:36 +0100

    Add some counts

--- notme.sh
+++ notme.sh
@@ -4,6 +4,12 @@ echo "Welcome to Not Me CLI!"
 
 read -p "What command would you like to run? " command
 
-if [[ $command == "create-not" || $command == "not" || $command == "new" ]]; then
+if [[ $command == "create-not" || $command == "not" || $command == "new" ]]
+then
   source $NOTME_PATH"/commands/create-not"
+elif [[ $command == "count-not" || $command == "count" ]]
+then
+  source $NOTME_PATH"/commands/count-not"
+else
+  echo "No command corresponds to \"${command}\"."
 fi
