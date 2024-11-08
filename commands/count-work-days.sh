b2ef4dfccee8790ec303605df822435c2b30fb94
gdarquie <gaetan.darquie@gmail.com>
2024-11-08 09:23:48 +0100

    Handle weeks in month

--- commands/count-work-days.sh
+++ commands/count-work-days.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
-
-# read -r -d '' WORKING_DAYS_SUMMARY << EOM
-# Week | Days   | working days           | count
-#  1   | 1-7    | Mo, Tu, We             | 3   
-#  2   | 8-15   | Mo, Tu                 | 2 
-#  3   | 16-23  | Mo                     | 1
-#  4   | 24-31  | Mo                     | 2 
-# EOM
-
-let result=0
+let working_days=0
 
 check_week_path() {
-  # WEEK_NUMBERS=(1 2 3 4 5)
-  # echo "here?"
-  # for accepted_week_number in ${WEEK_NUMBERS[@]}; do
+  WEEK_NUMBERS=(1 2 3 4 5)
+  for accepted_week_number in ${WEEK_NUMBERS[@]}; do
+    if [[ $1 == "$CURRENT_MONTH_FOLDER_PATH/$accepted_week_number" ]]; then
+      return 0;
+    fi
+  done
 
-  #   echo "week = $CURRENT_MONTH_FOLDER_PATH/$accepted_week_number"
-
-  #   if [[ $week=="$CURRENT_MONTH_FOLDER_PATH/$accepted_week_number" ]]; then
-  #     # return 1
-  #     echo "$CURRENT_MONTH_FOLDER_PATH/$accepted_week_number"
-  #   fi
-  # done
   return 1
-  # return $week ==  "$CURRENT_MONTH_FOLDER_PATH/1" || $week == "$CURRENT_MONTH_FOLDER_PATH/2" || $week == "$CURRENT_MONTH_FOLDER_PATH/3" || $week == "$CURRENT_MONTH_FOLDER_PATH/4" || $week == "$CURRENT_MONTH_FOLDER_PATH/5"
 }
 
 check_working_day() {
+  # todo: check in the file if it is a working day or not
   # add logic to check in comment if it is a working day
   # first we need to parse the content of the day
   # then I need to get the not in the beginning
@@ -36,7 +23,7 @@ check_working_day() {
 }
 
 increase_working_days_count() {
-  ((result++))
+  ((working_days++))
 }
 
 # get the current month = get the current year
@@ -46,18 +33,11 @@ CURRENT_MONTH=$(date +%m)
 # get the path of the current month folder
 CURRENT_MONTH_FOLDER_PATH=$NOT_PATH/$CURRENT_YEAR/$CURRENT_MONTH
 
-# get all weeks folder in current month, get all files for the month
-# echo "Week | Days   | working days           | count"
 
 is_week_path=1
 for week in $CURRENT_MONTH_FOLDER_PATH/*
 do
-  is_week_path=$(check_week_path "$week")
-  echo "is week is ="$is_week_path
-  if [[ $is_week_path -eq 0 ]]; then
-    # echo "$CURRENT_YEAR | x - y | X, X ,X | 1"
-    # echo 'week ='$week
-
+  if [[ $(check_week_path "$week") -eq 0 ]]; then
     for day in $week/*
     do
       # echo "day  ="$day
@@ -69,6 +49,4 @@ do
 done
 
 # in each files, check if it is a work day or not and if yes add it to the result
-
-echo "$WORKING_DAYS_SUMMARY"
-echo "Working day(s) this month: $result"
+echo "Working day(s) this month: $working_days"
